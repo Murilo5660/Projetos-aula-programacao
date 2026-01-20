7e8494dc35dd1048fae8126387b47a48db5208f0
Murilo Ferreira <murilo.stresser@escola.pr.gov.br>
2026-01-20 12:03:17 -0300

    Add files via upload

---
+++ Recuperação/Somatória Matriz.py
@@ -0,0 +1,12 @@
+matriz = [
+ [9 , 8 , 3],
+ [1 , 6 , 4],
+ [2 , 7 , 5]
+]
+soma_total = 0
+
+for linha in matriz:
+    for elemento in linha:
+        soma_total += elemento
+
+print(f"A soma total é: {soma_total}")
