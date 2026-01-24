f71d585db5534df6b4e73425791928fcc3bfde76
Murilo Ferreira <murilo.stresser@escola.pr.gov.br>
2026-01-24 12:03:07 -0300

    Update Nível Reservatório.py

--- Lista_Avançada/Nível Reservatório.py
+++ Lista_Avançada/Nível Reservatório.py
@@ -1,13 +1,10 @@
-nivel = float(input("Digite o nível do seu reservatório: "))
+nivel = float(input('Qual o nível de água do reservatório (%): '))
 
 if nivel < 20:
     print("Nível Crítico - Ligar Bomba de Emergência")
-
-elif nivel >= 20 and nivel <= 80:
+elif 20 >= nivel <= 80:
     print("Nível Operacional - Monitorar")
-
 elif nivel > 80 and nivel <= 100:
     print("Nível Alto - Desligar Entrada")
-
 else:
-    print("Porcentagem inválida")
+    print('Nível de água não aceito, digite novamente!')
