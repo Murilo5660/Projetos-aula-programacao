81c1df6b6d8600c70b6edf38a515b13cc0733b75
Murilo Ferreira <murilo.stresser@escola.pr.gov.br>
2026-01-24 11:44:03 -0300

    Update Diario Simples.py

--- Lista_Extra_de_Arquivos/Diario Simples.py
+++ Lista_Extra_de_Arquivos/Diario Simples.py
@@ -1,3 +1,22 @@
-frase = input("Como está se sentindo hoje ? ")
-with open ("diario.txt", "a") as diario:
-    diario.write(frase + "\n")
+# Exercício 1: Criar arquivo e escrever
+with open("Meu_Diário.txt", "w") as bloco:
+    bloco.write("Hoje, 3 de outubro de 2025, elaborei uma atividade em código sobre arquivo.txt.\n")
+
+print("Arquivo 'Meu_Diário.txt' criado com sucesso!")
+
+
+# Exercício 2: Ler o conteúdo do arquivo
+try:
+    with open("Meu_Diário.txt", "r") as bloco:
+        conteudo = bloco.read()
+        print(" --- Conteúdo do Diário ---")
+        print(conteudo)
+except FileNotFoundError:
+    print("O arquivo 'Meu_Diário.txt' ainda não existe. Rode o exercício 1 primeiro.")
+
+
+# Exercício 3: Acrescentar informação ao arquivo
+with open("Meu_Diário.txt", "a") as bloco:
+    bloco.write("Estou aprendendo a manipular arquivos e é muito útil.\n")
+
+print("Nova anotação no Meu Diário!")
