c8840cf32f73d777830da64403cb42246df936d2
Murilo Ferreira <murilo.stresser@escola.pr.gov.br>
2026-01-24 12:17:13 -0300

    Update and rename Validador de Senha.py to Validador de Senha e Usuário.py

---
+++ Introdução_VScode/Validador de Senha e Usuário.py
@@ -0,0 +1,12 @@
+senha = "1234"
+usuario = "Murilo"
+
+usuario_digitado = input("Digite seu nome de usuário: ")
+senha_digitada = input("Digite a senha: ")
+
+while usuario_digitado != usuario or senha_digitada != senha:
+    print("Acesso negado! Tente novamente...")
+    usuario_digitado = input("Digite seu nome de usuário: ")
+    senha_digitada = input("Digite a senha: ")
+
+print(f"Acesso concedido! Seja bem-vindo, usuário {usuario}!")
