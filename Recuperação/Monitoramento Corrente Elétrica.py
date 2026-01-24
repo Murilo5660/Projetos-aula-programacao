d1c34c8545bc278a9dfb4061f3dd63988998e266
Murilo Ferreira <murilo.stresser@escola.pr.gov.br>
2026-01-24 11:52:33 -0300

    Update Monitoramento Corrente Elétrica.py

--- Recuperação/Monitoramento Corrente Elétrica.py
+++ Recuperação/Monitoramento Corrente Elétrica.py
@@ -1,13 +1,14 @@
-correntes = [0.5 , 1.2 , 0.8 , 1.5 , 0.6 , 1.1 , 0.9 , 1.3]
+correntes = [0.5, 1.2, 0.8, 1.5, 0.6, 1.1, 0.9, 1.3]
 soma = 0
-leituras = 0
+for valor in correntes:
+    soma += valor 
 
-for i in correntes:
-    soma += i
-    if i >= 1.0:
-        leituras += 1
-    
-media = soma / len(correntes)
+media = soma/len(correntes)
+print(f'\nMédia das correntes: {media:.2f} A')
 
-print(f"A média das correntes medidas é: {media}")
-print(f"As leituras maiores que 1.0 são: {leituras}")
+contador = 0 
+for valor in correntes:
+    if valor > 1.0:
+        contador += 1
+
+print(f'Leituras acima de 1.0 A: {contador}')
