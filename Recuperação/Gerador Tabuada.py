bf846a51efe7fe8f605af16023f0f9f63bf79cfc
Murilo Ferreira <murilo.stresser@escola.pr.gov.br>
2026-01-24 11:51:08 -0300

    Update Gerador Tabuada.py

--- Recuperação/Gerador Tabuada.py
+++ Recuperação/Gerador Tabuada.py
@@ -1,8 +1,13 @@
-numero_tabuada = int(input("Digite o tipo de tabuada que desejar do 1 a 10: "))
-multiplicador = 0
+while True:
+    try:
+        numero = int(input('\nDigite um número inteiro positivo: '))
+        if numero > 0:
+            break
+        else:
+            print('erro: você deve digitar um número positivo!')
+    except ValueError :
+        print('Erro: você deve digitar um número inteiro!')
 
-if numero_tabuada < 0:
-        print("Tente novamente.")
-else:
-    for multiplicador in range(11):
-        print(numero_tabuada, " x " , multiplicador, " = ", (numero_tabuada*multiplicador))
+print(f'Tabuada do número {numero}:')
+for i in range(1,11):
+    print(f"{numero} x {i} = {numero * i}")
