74592b60f335af3cd2fee0e74c6572f412a508fa
Murilo Ferreira <murilo.stresser@escola.pr.gov.br>
2026-01-24 12:47:35 -0300

    Update Contador de Linhas.py

--- Lista_Extra_de_Arquivos/Contador de Linhas.py
+++ Lista_Extra_de_Arquivos/Contador de Linhas.py
@@ -1,3 +1,15 @@
-with open("diario.txt", "r") as diario:
-    linhas = diario.readlines()
-    print(len(linhas))
+nome = input("Digite o nome de um arquivo para contar as linhas |ex: perfil.txt|: ")
+with open(nome,"r") as arquivo:
+    linhas = arquivo.readlines()
+quantidade = len(linhas)
+print(f"O arquivo '{nome}' tem {quantidade} linhas")
+
+# Exercício 6 - Copiador de Arquivos
+print("\n--- Copiador de Arquivos ---")
+original = input("Digite o nome do arquivo que será copiado |ex: tarefa.txt|: ")
+copia = input("Digite o nome do novo arquivo |ex: copia.txt|: ")
+with open(original,"r") as orig:
+    conteudo = orig.read()
+with open(copia,"w") as copi:
+    copi.write(conteudo)
+print(f"Arquivo '{original}' copiado com sucesso para '{copia}'!")
