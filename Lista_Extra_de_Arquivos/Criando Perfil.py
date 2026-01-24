3a77056cb95f0474b886447b2cf67ca54d731b12
Murilo Ferreira <murilo.stresser@escola.pr.gov.br>
2026-01-24 12:44:09 -0300

    Update Criando Perfil.py

--- Lista_Extra_de_Arquivos/Criando Perfil.py
+++ Lista_Extra_de_Arquivos/Criando Perfil.py
@@ -1,4 +1,4 @@
-with open("perfil.txt", "w") as perfil:
-    perfil.write("Murilo\n")
-    perfil.write("18 anos\n")
-    perfil.write("Batata frita\n")
+with open("perfil.txt","w") as arquivo:
+    arquivo.write("Nome: Murilo\n")
+    arquivo.write("Idade: 16 anos\n")
+    arquivo.write("Comida preferida: Paçoca\n")
